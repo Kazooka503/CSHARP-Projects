57281f54a6b61d869d5fc14629d4321b4e65f66d
Kaz <austinkrz@gmail.com>
2020-07-20 15:04:19 -0700

    Car Insurance Approval Assignment
    Creates basic approval program for hypothetical car insurance. Asks users questions and returns true or false at the end to determine if applicant qualified.

---
+++ CarInsuranceApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            bool bool1 = dui == "yes";
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = age >= 15 && bool1 == false && ticket <= 3;
+            Console.WriteLine(Convert.ToString(qualified));
+            Console.ReadLine();
+
+        }
+    }
+}
