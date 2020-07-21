a4bbd328e993dfd10db2cadc30265f6d2199df39
Kaz <austinkrz@gmail.com>
2020-07-21 16:17:31 -0700

    Boolean Comparison Assignment
    Console app that utilizes a while statement and a do while statement.

---
+++ BooleanAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Defuse The Bomb!");
+        Console.WriteLine("Select a wire to cut");
+        Console.WriteLine("1=red, 2=green, 3=blue");
+        string guess = Console.ReadLine();
+        bool attemptFail = false;
+        bool gameWin = false;
+        //while (attemptCurrent == true)
+        do
+        {
+            switch (guess)
+            {
+                case "1":
+                case "red":
+                    attemptFail = true;
+                    break;
+                case "2":
+                case "green":
+                    gameWin = true;
+                    break;
+                case "3":
+                case "blue":
+                    attemptFail = true;
+                    break;
+            }
+        }
+        while (attemptFail == false && gameWin == false);
+
+        while (attemptFail == true)
+        {
+            Console.WriteLine("Wrong Wire. BOOM! Sorry, you lose");
+            Console.ReadLine();
+            break;
+        }
+
+        while (gameWin == true)
+        {
+            Console.WriteLine("You defused the bomb! You're the hero we need!");
+            Console.ReadLine();
+            break;
+        }
+
+    }
+}
