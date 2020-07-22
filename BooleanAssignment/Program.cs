560195a9687b170423e9504f409d9ba3158b9c57
Kaz <austinkrz@gmail.com>
2020-07-21 19:29:52 -0700

    Boolean Assignment Fix
    Update to Console app that utilizes a while statement and a do while statement. Gives possibility of each loop to iterate more than once, removes break statements from while loops and features a boolean evaluation to determine loop exit.

--- BooleanAssignment/Program.cs
+++ BooleanAssignment/Program.cs
@@ -8,42 +8,81 @@ class Program
         Console.WriteLine("Select a wire to cut");
         Console.WriteLine("1=red, 2=green, 3=blue");
         string guess = Console.ReadLine();
-        bool attemptFail = false;
-        bool gameWin = false;
-        //while (attemptCurrent == true)
+        bool gameWin = guess == "green";
+        
         do
         {
             switch (guess)
             {
                 case "1":
                 case "red":
-                    attemptFail = true;
+                    Console.WriteLine("Wrong Wire. There is still time left! Try again!");
+                    Console.WriteLine("1=red, 2=green, 3=blue");
+                    guess = Console.ReadLine();
                     break;
                 case "2":
                 case "green":
+                    Console.WriteLine("You defused the bomb! You're the hero we need!");
                     gameWin = true;
                     break;
                 case "3":
                 case "blue":
-                    attemptFail = true;
+                    Console.WriteLine("Wrong Wire. There is still time left! Try again!");
+                    Console.WriteLine("1=red, 2=green, 3=blue");
+                    guess = Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Your choice doesn't make sense.");
+                    Console.WriteLine("1=red, 2=green, 3=blue");
+                    guess = Console.ReadLine();
                     break;
             }
+ 
         }
-        while (attemptFail == false && gameWin == false);
+        while (!gameWin);
 
-        while (attemptFail == true)
-        {
-            Console.WriteLine("Wrong Wire. BOOM! Sorry, you lose");
-            Console.ReadLine();
-            break;
-        }
 
         while (gameWin == true)
         {
-            Console.WriteLine("You defused the bomb! You're the hero we need!");
-            Console.ReadLine();
-            break;
+            Console.WriteLine("Please Select Your Reward...");
+            Console.WriteLine("1=money, 2=fame, 3=karma");
+            string reward = Console.ReadLine();
+            bool end = reward == "karma";
+            while (!end)
+            {
+                switch (reward)
+                {
+                    case "1":
+                    case "money":
+                        Console.WriteLine("You are rewarded with endless riches, but you rethink your choice...");
+                        Console.WriteLine("Please Select Your Reward...");
+                        Console.WriteLine("1=money, 2=fame, 3=karma");
+                        reward = Console.ReadLine();
+                        break;
+                    case "2":
+                    case "fame":
+                        Console.WriteLine("You become known all over the Earth for your good deeds, but you rethink your choice...");
+                        Console.WriteLine("Please Select Your Reward...");
+                        Console.WriteLine("1=money, 2=fame, 3=karma");
+                        reward = Console.ReadLine();
+                        break;
+                    case "3":
+                    case "karma":
+                        Console.WriteLine("Your good deeds are reflected back onto you through the quantum realm");
+                        Console.ReadLine();
+                        end = true;
+                        break;
+                    default:
+                        Console.WriteLine("Your choice doen't make sense");
+                        Console.WriteLine("1=money, 2=fame, 3=karma");
+                        reward = Console.ReadLine();
+                        break;
+                }
+                if (end == true)
+                {
+                    Environment.Exit(0);
+                }
+            }
         }
-
     }
 }
