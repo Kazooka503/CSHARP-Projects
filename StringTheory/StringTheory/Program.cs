51b8dcfb22fecc01ffa8ce32ac9c6abb63b0add3
Kaz <austinkrz@gmail.com>
2020-07-22 13:28:40 -0700

    String Assignment
    Builds console app that concatenate's three strings, converts a tring to upper case, and utilizes StringBuilder to build a paragraph of text, once sentance at a time.

---
+++ StringTheory/StringTheory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace StringTheory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "In physics, " + "string theory is a theoretical framework in which the point-like particles of particle physics are replaced by" + " one-dimension objects called strings.";
+            string text2 = text.ToUpper();
+            Console.WriteLine(text);
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+            Console.WriteLine(text2);
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+
+
+            StringBuilder sb = new StringBuilder();
+            
+            sb.Append("On distance scales larger than the string scale, a string looks like any ordinary particle.");
+            sb.Append(" In string theory, one of the many vibrational states of the string corresponds to is the graviton, ");
+            sb.Append("a quantum mechanical particle that carries gravitational force.");
+            sb.Append(" Thus string theory is a thoery of quantum gravity.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+           
+            
+        }
+    }
+}
