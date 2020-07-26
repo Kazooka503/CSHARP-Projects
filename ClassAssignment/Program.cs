1f3d7f7607ffdfa76835c7a630c91d0c4929f203
Kaz <austinkrz@gmail.com>
2020-07-25 23:26:44 -0700

    Class Assignment
    Console at that utilizes classes and methods which take an integer parameter, do mathmatical operations on it, and return the results.

---
+++ ClassAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3;)
+            {
+                Console.WriteLine("What Number would you like to do math on?");
+                string entry = Console.ReadLine();
+                int number = Convert.ToInt32(entry);
+                while (i == 0)
+                {
+                    i++;
+                    int result = Calculate.Addition(number);
+                    Console.WriteLine(number + " plus " + 100 + " = " + result);
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+                }
+                while (i == 1)
+                {
+                    i++;
+                    int result2 = Calculate.Subtraction(number);
+                    Console.WriteLine(number + " minus " + 1 + " = " + result2);
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+                }
+                while (i == 2)
+                {
+                    i++;
+                    int result3 = Calculate.Multiplication(number);
+                    Console.WriteLine(number + " times " + 5 + " = " + result3);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ClassAssignment/test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Calculate
+    {
+        public static int Addition(int addend)
+        {
+            int sum = addend + 100;
+            return sum;
+        }
+
+        public static int Subtraction(int subtrahend)
+        {
+            int difference = subtrahend - 1;
+            return difference;
+        }
+
+        public static int Multiplication(int factor)
+        {
+            int product = factor * 5;
+            return product;
+        }
+    }
+}
