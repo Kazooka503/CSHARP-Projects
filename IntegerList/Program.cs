3cee1e91b64965f8ee3ad398490ee7800c284e7a
Kaz <austinkrz@gmail.com>
2020-07-25 19:36:59 -0700

    IntegerListConsoleApp
    Console App that creates a list of integers, asks user for a number to divide them by, and divides them by the number the user entered.
    
    Code is run thorugh try/catch block. Displays custom message if format exception is detected. Displays message to let user know program has emerged from try/catch block.

---
+++ IntegerList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numList = new List<int>() { 2, 3, 6 };
+                foreach (int number in numList)
+                {
+                    Console.WriteLine(number);
+                }
+                Console.WriteLine("Choose a number to divide by the numbers above");
+                int entry = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numList)
+                {
+                    int dividend = entry;
+                    int divisor = Convert.ToInt32(number);
+                    int quotient = dividend / divisor;
+                    Console.WriteLine(entry + " divided by " + divisor + " is " + quotient);
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine("Press Enter...");
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Please enter an actual number, ex: 1, 2, or 3");
+                Console.ReadLine();
+            }
+            finally
+            {
+                Console.ReadLine();
+                Console.WriteLine("Program Continuing...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
