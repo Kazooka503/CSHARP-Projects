c37b3a5632e2e5d4ec565beefb354afce1a0f2ab
Kaz <austinkrz@gmail.com>
2020-07-26 19:49:46 -0700

    ClassAssignment 2
    Console app that creates a class with three methods, one that takes an integer, one that takes a decimal, and one that takes a string as parameters- preforms addition on the user input, and returns results to screen.

---
+++ ClassAssignment2/ClassAssignment2/Calculate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment2
+{
+    class Calculate
+    {
+        public static int Addition(int addend)
+        {
+            int sum = 100 + addend;
+            return sum;
+        }
+
+        public static int Addition(decimal dec)
+        {
+            double number = 2.5;
+            decimal sum = Convert.ToDecimal(number) + dec;
+            int result = Convert.ToInt32(sum);
+            return result;
+        }
+        
+        public static int Addition(string addend)
+        {
+            int sum = Convert.ToInt32(addend) + 500;
+            return sum;
+        }
+    }
+}
---
+++ ClassAssignment2/ClassAssignment2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 1;)
+            {
+                Console.WriteLine("Pick a number to add 100 to");
+                string entry = Console.ReadLine();
+                i++;
+                int number = Convert.ToInt32(entry);
+                int result = Calculate.Addition(number);
+                Console.WriteLine(number + " plus " + 100 + " equals: " + result);
+                Console.ReadLine();
+            }
+            for (int j = 0; j < 1;)
+            {
+                j++;
+                Console.WriteLine("Choose a number to add 2.5 to");
+                string entry = Console.ReadLine();
+                decimal number = Convert.ToDecimal(entry);
+                int result2 = Calculate.Addition(number);
+                Console.WriteLine(number + " plus " + 2.5 + " equals: " + result2);
+                Console.ReadLine();
+            }
+            for (int j = 0; j < 1;)
+            {
+                j++;
+                Console.WriteLine("Choose a number to add 500 to.");
+                string entry = Console.ReadLine();
+                int result = Calculate.Addition(entry);
+                Console.WriteLine(entry + " plus " + 500 + " equals " + result);
+                Console.ReadLine();
+            }
+        }
+        
+    }
+}
