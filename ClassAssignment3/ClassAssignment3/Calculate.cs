abd9166888db0fec45f202ee38e5f9fdaa4e3941
Kaz <austinkrz@gmail.com>
2020-07-27 18:08:23 -0700

    ClassAssignment 3
    Program that creates a class, makes a method inside that class which performs subtraction on user input. Method takes two arguments, one is optional.

---
+++ ClassAssignment3/ClassAssignment3/Calculate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Remoting.Messaging;
+
+namespace ClassAssignment3
+{
+    class Calculate
+    {
+        public static Tuple<int,int> Subtract(int num1, int num2 = 0)
+        {
+            int result1, result2;
+            if (num2 == 0)
+            {
+                result1 = 100 - num1;
+                result2 = 0;
+            }
+            else
+            {
+                result1 = 100 - num1;
+                result2 = 100 - num2;
+            }
+            return new Tuple<int, int>(result1, result2);
+        }
+            
+    }
+}
+
---
+++ ClassAssignment3/ClassAssignment3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number. This number will be subtracted from 100");
+            int entry = Convert.ToInt32(Console.ReadLine());
+            try
+            { 
+                Console.WriteLine("If you desire, choose a second number that will be subtracted from 100");
+                int entry2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You selected " + entry + " " + entry2);
+                Console.WriteLine("Press Enter...");
+                Console.ReadLine();
+                var result = Calculate.Subtract(entry, entry2);
+                Console.WriteLine("Your calculations are: " + result);
+                Console.ReadLine();
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("You selected " + entry);
+                Console.WriteLine("Press Enter...");
+                Console.ReadLine();
+                var result = Calculate.Subtract(entry);
+                Console.WriteLine("Your calculations are: " + result);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
