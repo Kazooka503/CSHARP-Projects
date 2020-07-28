9440b48d7f35aaca01c1c68b00c5fe3377dd54c0
Kaz <austinkrz@gmail.com>
2020-07-27 20:10:26 -0700

    ClassAssignment 4
    Console App. Creates a class with a void method that takes two integers. Method preforms math on the first ineger and displays second integer to screen. Instanties the class traditionally and with parameter names.

---
+++ ClassAssignment4/Calculate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment4
+{
+    class Calculate
+    {
+        public static void Example (int num1, int num2)
+        {
+            int math = num1 + 1999;
+            Console.WriteLine("The Number Is: " + num2);
+        }
+    }
+}
---
+++ ClassAssignment4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ClassAssignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number");
+            int entry = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int entry2 = Convert.ToInt32(Console.ReadLine());
+            Calculate.Example(entry, entry2);
+            Console.ReadLine();
+            Console.WriteLine("Parameters by name");
+            Console.WriteLine("Press Enter... ");
+            Console.ReadLine();
+            Calculate.Example(num1: entry, num2: entry2);
+            Console.ReadLine();
+        }
+    }
+}
+
