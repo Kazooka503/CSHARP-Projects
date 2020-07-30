5a795e705642852cd2d5d188bbc5e3fb41e974f2
Kaz <austinkrz@gmail.com>
2020-07-30 14:17:48 -0700

    OverloadAssignment
    Console App. Overloads the "==" operator so it compares the Id property of two Employee objects.

---
+++ OverloadAssignment/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public Employee(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("ID: " + Id);
+        }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            if (emp1.Id == emp2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            if (emp1.Id != emp2.Id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+
+    
+
+    
+}
---
+++ OverloadAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace OverloadAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(1984);
+            employee.Display();
+
+            Employee employee2 = new Employee(2187);
+            employee2.Display();
+
+            if (employee == employee2)
+            {
+                Console.WriteLine("Employee is equal to Employee2");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not equal to Employee2");
+            }
+
+            Console.ReadLine();
+            
+        }  
+    }
+}
