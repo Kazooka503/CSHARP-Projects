88f59d11d7061454878d8f0b2cbf206a13e045c8
Kaz <austinkrz@gmail.com>
2020-07-30 17:16:23 -0700

    GenericAssignment
    Console App. Creates Employee class that takes generic type parameter. Adds generic list called things to Employee class. Instantiates an Employee object with type "string" as its generic parameter. Assigns list of strings as property value of Things.
    
    Instantiates an Employee object with type "int" as its generic paramter. Assigns a list of integers as the property value of things.
    
    Creates loop that prints all of the Things to the Console.

---
+++ GenericAssignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericAssignment
+{
+    public class Employee<T>
+    {
+        public List<T> things = new List<T>();     
+        
+    }
+}
---
+++ GenericAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfStrings = new List<string>() { "String 1", "String 2", "String 3" };
+            Employee<string> employee = new Employee<string>();
+
+            foreach(string entry in listOfStrings)
+            {
+                employee.things.Add(entry);   
+            }
+            foreach(string entry in employee.things)
+            {
+                Console.WriteLine(entry);     
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Press Enter...");
+            Console.ReadLine();
+
+            List<int> listOfInts = new List<int>() { 3, 6, 9 };
+            Employee<int> employee2 = new Employee<int>();
+
+            foreach(int entry in listOfInts)
+            {
+                employee2.things.Add(entry);
+            }
+            foreach (int entry in employee2.things)
+            {
+                Console.WriteLine("Int: " + entry);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
