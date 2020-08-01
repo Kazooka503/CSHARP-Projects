39b8dd459dd25c1ebe92ccc05951ada0df66ac61
Kaz <austinkrz@gmail.com>
2020-08-01 14:09:46 -0700

    EnumAssignment
    Console app. Creates enum for the days of the week. Prompts user to enter current day. Assigns value to a variable of that enum data type. Wraps statement in Try/Catch block and prints error message to screen if error occurs.

---
+++ EnumAssignment/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+namespace EnumAssignment
+{
+    class Program
+    {
+
+        public enum Week
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Day Of The Week Is It?");
+            string entry = Console.ReadLine();
+            
+            try
+            {
+                Week newWeek = (Week)Enum.Parse(typeof(Week), entry);
+                if (newWeek == Week.Monday)
+                {
+                    Console.WriteLine(newWeek.ToString());
+                    Console.ReadLine();
+                }
+                else if (newWeek == Week.Tuesday)
+                {
+                    Console.WriteLine(newWeek.ToString());
+                    Console.ReadLine();
+                }
+                else if (newWeek == Week.Wednesday)
+                {
+                    Console.WriteLine(newWeek.ToString());
+                    Console.ReadLine();
+                }
+                else if (newWeek == Week.Thursday)
+                {
+                    Console.WriteLine(newWeek.ToString());
+                    Console.ReadLine();
+                }
+                else if (newWeek == Week.Friday)
+                {
+                    Console.WriteLine(newWeek.ToString());
+                    Console.ReadLine();
+                }
+                else if (newWeek == Week.Saturday)
+                {
+                    Console.WriteLine(newWeek.ToString());
+                    Console.ReadLine();
+                }
+                else if (newWeek == Week.Sunday)
+                {
+                    Console.WriteLine(newWeek.ToString());
+                    Console.ReadLine();
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                Console.ReadLine();
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                Console.ReadLine();
+            }
+           
+        }
+
+    }
+}
