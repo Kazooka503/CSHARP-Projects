ca8980617a1e9592e8a2d3a94c4dae2fb52a927f
Kaz <austinkrz@gmail.com>
2020-08-01 15:18:07 -0700

    StructAssignment
    Console app. Creates struct called Number and assigns int property called Amount. Creates a Number object in main method and assigns an amount to it. Prints amount to console.

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace StructAssignment
+{
+    struct Number
+    {
+        public int Amount;  
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 107;
+            Console.WriteLine("The number is: " + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
