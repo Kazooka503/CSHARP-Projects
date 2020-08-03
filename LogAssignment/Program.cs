4cf923586b879633ee6d9416cf847746f582e21e
Kaz <austinkrz@gmail.com>
2020-08-03 16:51:43 -0700

    LogAssignment
    Console app. Asks user for a number. Logs number to text file. Prints the text file back to the user.

---
+++ LogAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your number selection?");
+            int entry = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\austi\desktop\log.txt", true))
+            {
+                file.WriteLine(entry);
+            }
+
+            string text = File.ReadAllText(@"C:\Users\austi\desktop\log.txt");
+
+            Console.WriteLine("");
+            Console.WriteLine(text);
+            Console.ReadLine();
+
+
+        }
+    }
+}
