e173bafaf3c108945c4b367a2f0b9466e83576a6
Kaz <austinkrz@gmail.com>
2020-08-05 00:06:50 -0700

    DateTimeAssignment
    Console App. Prints current date and time to console. Asks user for a number and prints the exact time it will be in hours equivilent to number entered.

---
+++ DatetimeAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Today's date and time is: " + DateTime.Now);
+            Console.WriteLine(" ");
+            Console.WriteLine("Please Enter a Number...");
+            string entry = Console.ReadLine();
+            int result = Convert.ToInt32(entry);
+            DateTime today = DateTime.Now;
+            TimeSpan duration = new TimeSpan(result, 0, 0);
+            DateTime answer = today.Add(duration);
+            Console.WriteLine(entry + " hours from now will be " + answer);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
