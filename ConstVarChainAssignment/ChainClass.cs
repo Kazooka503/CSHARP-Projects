fb04b119528ea41b79b6d14c4816e4db5ef7904a
Kaz <austinkrz@gmail.com>
2020-08-06 15:16:34 -0700

    ConstVarChain Assignment
    Console app. Creates a const variable. Creates a variable using the keyword var.  Chains two constructors together.

---
+++ ConstVarChainAssignment/ChainClass.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ConstVarChainAssignment
+{
+    public class ChainClass
+    {
+        public int Id;
+        public string Name;
+        //chaining two constructors
+        public ChainClass(int id)
+        {
+            this.Id = id;
+        }
+
+        public ChainClass(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ ConstVarChainAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace ConstVarChainAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const 
+            const string example = "lorem ipsum";
+            Console.WriteLine(example);
+            Console.ReadLine();
+            //var
+            int x = 1;
+            int y = 3;
+            var z = x + y;
+            Console.WriteLine(x + " plus " + y + " equals " + z);
+            Console.ReadLine();
+            //chain
+            var classExample = new ChainClass(1, "Joe");
+            Console.WriteLine("ID number is: " + classExample.Id + " Name is: " + classExample.Name);
+            Console.ReadLine();
+        }
+    }
+}
