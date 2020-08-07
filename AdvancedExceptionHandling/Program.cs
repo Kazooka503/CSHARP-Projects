4eb8a7cd7b649bfb70dc7fb8b8e2018de33e5c32
Kaz <austinkrz@gmail.com>
2020-08-06 19:55:11 -0700

    AdvancedExceptionHandling
    Console app. Asks for users age. Displays the year the user was born. Sets up Try/Catch block. Displays appropraite error message if user enters negative number or zero.
    Displays a general error message caused by anything else.

---
+++ AdvancedExceptionHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AdvancedExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What is your age?");
+                int entry = Convert.ToInt32(Console.ReadLine());
+                if (entry <= 0)
+                {
+                    throw new FormatException();
+                }
+                DateTime current = DateTime.Now;
+                int year = current.Year;
+                int b_y = year - entry;
+                Console.WriteLine("Your birth year is: " + b_y);
+                Console.ReadLine();
+   
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number greater than zero.");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured. Please restart application.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
